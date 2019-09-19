eb659051fe08bf3402a35b3dab688c7329a1ae83
Ensup Choi <ensup@outlook.com>
2019-09-19 23:05:59 +0900

    main.c 소스코드 수정

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(void){
-    printf("램을 선택해 주세요"\n);
-    printf("512MB\n");
-    printf("1GB\n");
-    printf("2GB\n");
-    printf("3GB\n");
+    printf("1. 512MB\n");
+    printf("2. 1GB~4GB\n");
+    printf("3. 2GB~8GB\n");
+    printf("램을 선택해 주세요:");
+    int ram = getchar();
+    if(ram==1){
+        system("java -Xms512MB -Xmx512MB -jar server.jar")
+    }
 }
