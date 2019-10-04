8db452c3250c9ad45fa225f5d7ef1076e08748b7
Ensup Choi <ensup@outlook.com>
2019-10-04 14:01:25 +0000

    main.c edited online with Bitbucket

--- main.c
+++ main.c
@@ -10,16 +10,16 @@ int main(void){
     switch (ram)
     {
     case 1:
-        system("java -Xms512MB -Xmx512MB -jar server.jar");
+        system("java -Xms512MB -Xmx512MB -jar server.jar nogui");
         break;
     case 2:
-        system("java -Xms1GB -Xmx1GB -jar server.jar");
+        system("java -Xms1GB -Xmx1GB -jar server.jar nogui");
         break;
     case 3:
-        system("java -Xms1GB -Xmx4GB -jar server.jar");
+        system("java -Xms1GB -Xmx4GB -jar server.jar nogui");
         break;
     case 4:
-        system("java -Xms2GB -Xmx8GB -jar server.jar");
+        system("java -Xms2GB -Xmx8GB -jar server.jar nogui");
         break;
     default:
         printf("Please type correctly\n");
